e923969edac495de756603fbfaf998e18a21e844
Asif Ansari <asif.basecamp@gmail.com>
2019-08-08 17:08:55 +0530

    New UI changes

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -12,4 +12,8 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
 
+  public listItems: Array<string> = [
+    'Baseball', 'Basketball', 'Cricket', 'Field Hockey',
+    'Football', 'Table Tennis', 'Tennis', 'Volleyball'
+  ];
 }
